f99730c0587f973d3d404b2a2f592483c039ffe2
Matt Duncan <mduncan@fulfillment.com>
2020-06-25 15:41:56 -0400

    Implement http logging to console
    * To help those who need to see everything
    * Replace node-fetch with http library used by oauth client

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const ClientOAuth2 = require('client-oauth2');
 const express = require('express');
-const fetch = require('node-fetch');
+const popsicle = require('popsicle');
 const app = express();
+const clientRequest = require('client-oauth2/src/request');
+
+const loggingRequest = (method, url, body, headers) => {
+    console.log({
+        method,
+        url,
+        body,
+        headers
+    });
+    return clientRequest(method, url, body, headers);
+}
 
 const fdcBaseUrl = process.env.fdcApiUri;
 
@@ -12,11 +23,12 @@ const fdcAuth = new ClientOAuth2({
     authorizationUri: process.env.authorizationUri,
     redirectUri: process.env.redirectUri,
     scopes: ['oms']
-});
+}, loggingRequest);
 
 app.get('/auth/fdc', function (req, res) {
     const uri = fdcAuth.code.getUri();
 
+    console.log(`Redirecting to ${uri} to allow the user to login and receive an auth code`);
     res.redirect(uri)
 })
 
@@ -26,25 +38,34 @@ app.get('/auth/fdc/callback', async function (req, res) {
     // Store the `access_token` and `refresh_token` token somewhere
     // and optionally store `expires` if you would like to optimistically
     // refresh the token before it expires
-    console.log(user.data) //=> { access_token: '...', token_type: 'bearer', ... }
+    console.log('Authorization Response', user.data); //=> { access_token: '...', token_type: 'bearer', ... }
 
-        // Example of refreshing a token
-        // which should be used when an access token returns 401 because it has expired
-        user = await user.refresh();
+    // Example of refreshing a token
+    // which should be used when an access token returns 401 because it has expired
+    user = await user.refresh();
 
-        const userInfoFromApiResponse = await fetch(`${fdcBaseUrl}/users/me`, {
-            method: 'get',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${user.data.access_token}`
-            }
-        });
-        const userInfo = await userInfoFromApiResponse.json();
+    // Get user info from FDC API
+    console.log({
+        url: `${fdcBaseUrl}/users/me`,
+        header: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${user.data.access_token}`
+        },
+        method: 'GET'
+    })
+    const userInfoFromApiResponse = await popsicle.fetch(`${fdcBaseUrl}/users/me`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${user.data.access_token}`
+        }
+    });
+    const userInfo = await userInfoFromApiResponse.json();
 
-        const responseData = {
-            auth: user.data,
-            user: userInfo
-        };
+    const responseData = {
+        auth: user.data,
+        user: userInfo
+    };
 
     return res.send(JSON.stringify(responseData));
 })
