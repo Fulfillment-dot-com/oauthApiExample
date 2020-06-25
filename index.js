9b9549188748f3de5ca0353acd23526253e49b07
Matt Duncan <mduncan@fulfillment.com>
2020-06-25 14:53:55 -0400

    Include example request to api using auth response

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
-var ClientOAuth2 = require('client-oauth2')
-var express = require('express')
-var app = express()
+const ClientOAuth2 = require('client-oauth2');
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
 
-var fdcAuth = new ClientOAuth2({
+const fdcBaseUrl = process.env.fdcApiUri;
+
+const fdcAuth = new ClientOAuth2({
     clientId: process.env.clientId,
     clientSecret: process.env.clientSecret,
     accessTokenUri: process.env.accessTokenUri,
     authorizationUri: process.env.authorizationUri,
     redirectUri: process.env.redirectUri,
     scopes: ['oms']
-})
+});
 
 app.get('/auth/fdc', function (req, res) {
-    var uri = fdcAuth.code.getUri()
+    const uri = fdcAuth.code.getUri();
 
     res.redirect(uri)
 })
 
-app.get('/auth/fdc/callback', function (req, res) {
-    fdcAuth.code.getToken(req.originalUrl)
-        .then(function (user) {
-            console.log(user) //=> { accessToken: '...', tokenType: 'bearer', ... }
+app.get('/auth/fdc/callback', async function (req, res) {
+    let user = await fdcAuth.code.getToken(req.originalUrl);
 
-            // Refresh the current users access token.
-            user.refresh().then(function (updatedUser) {
-                console.log(updatedUser !== user) //=> true
-                console.log(updatedUser.accessToken)
-            })
+    // Store the `access_token` and `refresh_token` token somewhere
+    // and optionally store `expires` if you would like to optimistically
+    // refresh the token before it expires
+    console.log(user.data) //=> { access_token: '...', token_type: 'bearer', ... }
 
-            // We should store the token into a database.
-            return res.send(JSON.stringify(user.data))
-        })
+        // Example of refreshing a token
+        // which should be used when an access token returns 401 because it has expired
+        user = await user.refresh();
+
+        const userInfoFromApiResponse = await fetch(`${fdcBaseUrl}/users/me`, {
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.data.access_token}`
+            }
+        });
+        const userInfo = await userInfoFromApiResponse.json();
+
+        const responseData = {
+            auth: user.data,
+            user: userInfo
+        };
+
+    return res.send(JSON.stringify(responseData));
 })
 
 app.listen(8001, 'localhost');
